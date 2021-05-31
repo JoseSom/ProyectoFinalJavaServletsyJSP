af512d7df60f6522e5bf47a1e14a8a89131bebcb
Jose Somilleda <joseluissom004@gmail.com>
2021-05-31 15:00:53 -0500

    Se agrego la clase Cliente con el patron de diseño DAO

--- src/main/java/domain/Cliente.java
+++ src/main/java/domain/Cliente.java
@@ -1,13 +1,37 @@
 package domain;
 
-
 public class Cliente {
+
     private int idCliente;
     private String nombre;
     private String apellido;
     private String email;
     private String telefono;
     private double saldo;
+
+    public Cliente() {
+    }
+
+    public Cliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Cliente(String nombre, String apellido, String email, String telefono, double saldo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+        this.saldo = saldo;
+    }
+
+    public Cliente(int idCliente, String nombre, String apellido, String email, String telefono, double saldo) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+        this.saldo = saldo;
+    }
 
     public int getIdCliente() {
         return idCliente;
@@ -56,6 +80,18 @@ public class Cliente {
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-    
-    
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cliente{idCliente=").append(idCliente);
+        sb.append(", nombre=").append(nombre);
+        sb.append(", apellido=").append(apellido);
+        sb.append(", email=").append(email);
+        sb.append(", telefono=").append(telefono);
+        sb.append(", saldo=").append(saldo);
+        sb.append('}');
+        return sb.toString();
+    }
+
 }
